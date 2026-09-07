e042e4b77746e727e6d4d1ca204fd6f60e28de91
Adib Raiyan <adibraiyanruhsaf@gmail.com>
2026-09-08 04:23:09 +0600

    feat: create http server on port 5000

--- index.js
+++ index.js
@@ -5,31 +5,7 @@ const path = require("path");
 let filePath = "";
 let statusCode = 200;
 
-const server = http.createServer((req, res) => {
-  if (req.url == "/") {
-    filePath = path.join(__dirname, "views", "home.html");
-  } else if (req.url == "/blog") {
-    filePath = path.join(__dirname, "views", "blog.html");
-  } else if (req.url == "/about") {
-    filePath = path.join(__dirname, "views", "about.html");
-  } else if (req.url == "/contact") {
-    filePath = path.join(__dirname, "views", "contact.html");
-  } else {
-    filePath = path.join(__dirname, "views", "error.html");
-    statusCode = 404;
-  }
-
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(500, { "content-type": "text/plain" });
-      res.end("Internal Server Error.");
-      return;
-    }
-
-    res.writeHead(statusCode, { "content-type": "text/html" });
-    res.end(data);
-  });
-});
+const server = http.createServer((req, res) => {});
 
 server.listen(5000, () => {
   console.log("Server is running on PORT 5000.");

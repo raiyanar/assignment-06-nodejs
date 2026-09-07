2d93f927ca0d3d32c7bcbe9b1395425321953dab
Adib Raiyan <adibraiyanruhsaf@gmail.com>
2026-09-08 01:40:28 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,49 @@
+/* 
+Node.js ব্যবহার করে একটি Simple HTTP Server তৈরি করতে হবে।
+Requirements
+1. Create HTTP Server
+http module ব্যবহার করে একটি Server তৈরি করতে হবে।
+Server অবশ্যই Port 5000 এ Run করবে।
+*/
+
+/*
+2. Create Routes
+নিচের Route গুলো তৈরি করতে হবে।
+Route - Output
+/ - Home Page
+/blog - Blog Page
+/contact - Contact Page
+/about - About Page
+*/
+
+/*  
+3. HTML Response
+প্রতিটি Route-এ HTML Content Send করতে হবে।
+Example:
+Home Page
+Blog Page
+Contact Page
+About Page
+প্রতিটি Page-এ একটি Heading, কিছু Description এবং Navigation Link থাকতে হবে।
+*/
+
+/*  
+4. Invalid Route
+যদি অন্য কোনো Route-এ যায় তাহলে 404 - Page Not Found দেখাতে হবে। 
+*/
+
+/*  
+GitHub Submission Rules
+Repository Name
+assignment-06-nodejs
+Commit
+কমপক্ষে ৫টি Meaningful Commit থাকতে হবে।
+Example:
+Initialize Node Project
+Create HTTP Server
+Add Home Route
+Add Blog, About  & Contact Route
+Complete Assignment
+Submission Rules: 
+Git এ  পুশ করে রিপোজিটরি লিংক দিতে হবে। অবশ্যই লিংক পাবলিক রাখতে হবে। Git Link  ছাড়া যদি আপনি সরাসরি zip আকারে জমা করেন তাহলে 0 পাবেন।  
+*/
